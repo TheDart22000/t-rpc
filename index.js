c78c0d61764f137a16c1e81a1444b7d40880f74a
Taylor <121888944+TheDart22000@users.noreply.github.com>
2024-09-24 08:42:28 +0100

    Update index.js
    + Middleware

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const PORT = 5000;
 const app = express();
 
+app.use((req, res, next) => {
+    const reqData = {
+	    method: req.method,
+	    targetUrl: req.url,
+	    clientIp: req.ip
+    };
+
+    console.log(`[Server/Middleware]: ${reqData.method} request recieved - recieved from: ${reqData.clientIp} - target: ${reqData.targetUrl}`);
+    // Tell route to complete.
+   next();
+});
+
 app.get('/', (req, res) => {
 	res.send('Hello!')
 });
